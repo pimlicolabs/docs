aac53ed209f0b4631773aafa3185bff5d721fe78
Sergey Potekhin <sergey@pimlico.io>
2024-06-11 20:07:09 +0400

    Add trustwallet docs (#23)
    * Add Trust Account related docs
    
    * Add Trust Account related code snippets
    
    * Add Trust Account docs to vocs
    
    * Update version to v0.6 for Trust Account
    
    * Update lock
    
    * Fix header
    
    ---------
    
    Co-authored-by: PowerStream3604 <63450340+PowerStream3604@users.noreply.github.com>

---
+++ docs/snippets/signers/accounts/trustSmartAccount.ts
@@ -0,0 +1,19 @@
+const smartAccountSigner = privateKeyToAccount(generatePrivateKey())
+
+import { ENTRYPOINT_ADDRESS_V06 } from "permissionless"
+// [!region main]
+import { signerToTrustSmartAccount } from "permissionless/accounts"
+import { createPublicClient, http } from "viem"
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
+import { sepolia } from "viem/chains"
+
+export const publicClient = createPublicClient({
+	transport: http("https://rpc.ankr.com/eth_sepolia"),
+	chain: sepolia,
+})
+
+const smartAccount = await signerToTrustSmartAccount(publicClient, {
+	signer: smartAccountSigner,
+	entryPoint: ENTRYPOINT_ADDRESS_V06,
+})
+// [!endregion main]
