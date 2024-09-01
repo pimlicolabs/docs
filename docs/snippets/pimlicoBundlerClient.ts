6ae593747e80cfbd0c468016df5c63103bdb6f84
Garvit Khatri <garvitdelhi@gmail.com>
2024-09-01 13:59:14 +0100

    remove use of old permissionless

--- docs/snippets/pimlicoBundlerClient.ts
+++ docs/snippets/pimlicoBundlerClient.ts
@@ -1,13 +1,15 @@
 // [!region client]
-import { ENTRYPOINT_ADDRESS_V07, bundlerActions } from "permissionless"
-import { pimlicoBundlerActions } from "permissionless/actions/pimlico"
-import { createClient, http } from "viem"
+import { createPimlicoClient } from "permissionless/clients/pimlico"
+import { http } from "viem"
+import { entryPoint07Address } from "viem/account-abstraction"
 import { sepolia } from "viem/chains"
 
-export const pimlicoBundlerClient = createClient({
+export const pimlicoClient = createPimlicoClient({
 	chain: sepolia,
+	entryPoint: {
+		address: entryPoint07Address,
+		version: "0.7",
+	},
 	transport: http("https://api.pimlico.io/v2/sepolia/rpc?apikey=YOUR_API_KEY_HERE"),
 })
-	.extend(bundlerActions(ENTRYPOINT_ADDRESS_V07))
-	.extend(pimlicoBundlerActions(ENTRYPOINT_ADDRESS_V07))
 // [!endregion client]
