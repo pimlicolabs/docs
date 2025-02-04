d898caefe41b270833edc5e9736e617d7453b764
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2025-02-04 21:09:59 +0000

    update sdk

--- docs/snippets/signers/para.ts
+++ docs/snippets/signers/para.ts
@@ -1,15 +1,19 @@
 // [!region main]
-import Capsule from "@usecapsule/web-sdk"
-import { createCapsuleViemClient } from "@usecapsule/viem-v2-integration"
+import Para from "@getpara/web-sdk"
+import { createParaViemClient } from "@getpara/viem-v2-integration"
+import { toSafeSmartAccount } from "permissionless/accounts"
 import { http } from "viem"
 import { sepolia } from "viem/chains"
 
-// Param options here will be specific to your project.  See the Capsule docs for more info.
-const capsule = new Capsule(env, apiKey)
+// Param options here will be specific to your project.  See the Para docs for more info.
+const para = new Para(env, apiKey)
 
-// Follow the Capsule docs for more instructions on creating the Viem client https://docs.usecapsule.com/integration-guide/signing-transactions
-const smartAccountOwner = createCapsuleViemClient(capsule, {
+// Convert a Para viem client to a SmartAccountSigner
+// Follow the Para docs for more instructions on creating the Viem client https://docs.getpara.com/integration-guide/signing-transactions
+const viemClient = createParaViemClient(para, {
 	chain: sepolia,
 	transport: http("https://rpc.ankr.com/eth_sepolia"),
 })
+
+const smartAccountSigner = toSafeSmartAccount({ owners: [viemClient] })
 // [!endregion main]
--- docs/snippets/v0_1/signers/para.ts
+++ docs/snippets/v0_1/signers/para.ts
@@ -1,16 +1,16 @@
 // [!region main]
-import Capsule from "@usecapsule/web-sdk"
-import { createCapsuleViemClient } from '@usecapsule/viem-v2-integration';
+import Para from "@getpara/web-sdk"
+import { createParaViemClient } from "@getpara/viem-v2-integration"
 import { walletClientToSmartAccountSigner } from "permissionless"
 import { http } from "viem"
 import { sepolia } from "viem/chains"
 
-// Param options here will be specific to your project.  See the Capsule docs for more info.
-const capsule = new Capsule(env, apiKey)
+// Param options here will be specific to your project.  See the Para docs for more info.
+const para = new Para(env, apiKey)
 
-// Convert a Capsule viem client to a SmartAccountSigner
-// Follow the Capsule docs for more instructions on creating the Viem client https://docs.usecapsule.com/integration-guide/signing-transactions
-const viemClient = createCapsuleViemClient(capsule, {
+// Convert a Para viem client to a SmartAccountSigner
+// Follow the Para docs for more instructions on creating the Viem client https://docs.getpara.com/integration-guide/signing-transactions
+const viemClient = createParaViemClient(para, {
 	chain: sepolia,
 	transport: http("https://rpc.ankr.com/eth_sepolia"),
 })
