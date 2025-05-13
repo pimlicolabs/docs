8b611eeadb06933588a21a39025ac3543eddb519
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2025-05-13 15:09:34 +0100

    update conditional sponsorships guide

--- docs/snippets/how-to/paymasters/conditional-sponsoring.ts
+++ docs/snippets/how-to/paymasters/conditional-sponsoring.ts
@@ -1,5 +1,5 @@
 // [!region client]
-import { createPublicClient, http } from "viem"
+import { createPublicClient, Hex, http, zeroAddress } from "viem"
 import { decodeNonce } from "permissionless/utils"
 import { createPimlicoClient } from "permissionless/clients/pimlico"
 import { entryPoint07Address } from "viem/account-abstraction"
@@ -44,56 +44,41 @@ const smartAccountClient = createSmartAccountClient({
 	account: simpleSmartAccount,
 	chain: sepolia,
 	bundlerTransport: http(pimlicoUrl),
-	paymaster: {
-		async getPaymasterData(parameters) {
-			const nonce = await getAccountNonce(publicClient, {
-				address: parameters.sender,
-				entryPointAddress: parameters.entryPointAddress,
-			})
-
-			const { sequence } = decodeNonce(nonce)
-
-			if (sequence < 10) {
-				// sponsor it for the user as the sequence is less than 10
-				// sequence is the number of transactions sent by the user for a given key
-				// by default key is 0x0
-				return pimlicoClient.getPaymasterData(parameters)
-			}
-
-			return {
-				paymaster: "0x",
-				paymasterData: "0x",
-				paymasterVerificationGasLimit: 0n,
-				paymasterPostOpGasLimit: 0n,
-			}
-		},
-		async getPaymasterStubData(parameters) {
-			const nonce = await getAccountNonce(publicClient, {
-				address: parameters.sender,
-				entryPointAddress: parameters.entryPointAddress,
-			})
-
-			const { sequence } = decodeNonce(nonce)
-
-			if (sequence < 10) {
-				// sponsor it for the user as the sequence is less than 10
-				// sequence is the number of transactions sent by the user for a given key
-				// by default key is 0x0
-				return pimlicoClient.getPaymasterStubData(parameters)
-			}
-
-			return {
-				paymaster: "0x",
-				paymasterData: "0x",
-				paymasterVerificationGasLimit: 0n,
-				paymasterPostOpGasLimit: 0n,
-			}
-		},
-	},
 	userOperation: {
 		estimateFeesPerGas: async () => {
 			return (await pimlicoClient.getUserOperationGasPrice()).fast
 		},
 	},
 })
+
+// Only sponsor userOps with nonce sequence < 10
+const nonce = await getAccountNonce(publicClient, {
+	address: simpleSmartAccount.address,
+	entryPointAddress: entryPoint07Address,
+})
+const { sequence } = decodeNonce(nonce)
+
+let txHash: Hex
+if (sequence < 10) {
+    txHash = await smartAccountClient.sendTransaction({
+    	calls: [
+    		{
+    			to: zeroAddress,
+    			value: 0n,
+    			data: "0x",
+    		},
+    	],
+    	paymaster: pimlicoClient // [!code ++]
+    });
+} else {
+	txHash = await smartAccountClient.sendTransaction({
+		calls: [
+			{
+				to: zeroAddress,
+				value: 0n,
+				data: "0x",
+			},
+		]
+	});
+}
 // [!endregion smart-account-client]
